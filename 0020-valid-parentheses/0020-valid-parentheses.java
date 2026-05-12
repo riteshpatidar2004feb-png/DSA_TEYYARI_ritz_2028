eb0930f62ff65d2e97d6ce143d412ac211b1804a
Ritesh patidar <riteshpatidar2004feb@gmail.com>
2026-05-12 21:07:18 +0530

    Time: 3 ms (87.78%), Space: 43.1 MB (84.62%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,19 @@
-import java.util.Stack;
-
 class Solution {
-    public boolean isValid(String s){
-        Stack<Character> stack=new Stack<>();
-        for(char ch:s.toCharArray()){
-            if(ch=='('||ch=='{'||ch=='['){
-                stack.push(ch);
-            } 
+    public boolean isValid(String s) {
+        if(s.length()%2 != 0) return false;
+
+        Stack <Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='[') st.push(ch);
             else{
-                if(stack.isEmpty()) return false;
-                char top=stack.pop();
-                if((ch==')'&&top!='(')||
-                    (ch=='}'&&top!='{')||
-                    (ch==']'&&top!='[')){
-                    return false;
-                }
+                if(st.size() == 0) return false;
+                char top = st.peek();
+                if((top == '('&& ch == ')')||(top == '['&& ch == ']')||(top == '{'&& ch == '}')) st.pop();
+                else return false;
             }
         }
-        return stack.isEmpty();
+        if(st.size()==0) return true;
+        return false;
     }
 }
