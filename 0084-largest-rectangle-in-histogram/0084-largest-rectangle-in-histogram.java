4cd213dd07fe1ab036c69959cab276c68b0176be
Ritesh patidar <riteshpatidar2004feb@gmail.com>
2026-05-28 22:07:05 +0530

    Time: 108 ms (21.57%), Space: 76.2 MB (89.33%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -33,3 +33,46 @@ class Solution {
         return area;
     }
 }
+
+
+//         class Solution {
+//     public int largestRectangleArea(int[] heights) {
+//         int[] pse = PSE(heights);
+//         int[] nse = NSE(heights);
+//         int area = Integer.MIN_VALUE;
+//         for(int i = 0; i < pse.length; i++){
+//             int a = heights[i] * (nse[i] - pse[i] - 1);
+//             if(a > area) area = a;
+//         }
+//         return area;
+//     }
+//     public int[] PSE(int[] heights){
+//         int n = heights.length;
+//         int[] answer = new int[n];
+//         Stack <Integer> st = new Stack<>();
+//         st.push(0);
+//         answer[0] = -1;
+//         for(int i = 1; i < n; i++){
+//             while(st.size() > 0 && heights[i] <= heights[st.peek()]) st.pop();
+//             if(st.size() == 0) answer[i] = -1;
+//             else answer[i] = st.peek();
+//             st.push(i);
+//         }
+//         return answer;
+//     }
+//     public int[] NSE(int[] heights){
+//         int n = heights.length;
+//         int[] answer = new int[n];
+//         Stack <Integer> st = new Stack<>();
+//         st.push(n-1);
+//         answer[n-1] = n;
+//         for(int i = n-2; i >= 0; i--){
+//             while(st.size() > 0 && heights[i] <= heights[st.peek()]) st.pop();
+//             if(st.size() == 0) answer[i] = n;
+//             else answer[i] = st.peek();
+//            st.push(i);
+//         }
+//         return answer;
+//     }
+// }
+    
